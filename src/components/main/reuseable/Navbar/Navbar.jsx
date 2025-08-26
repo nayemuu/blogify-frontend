dcf9ba5196a09d6ac030d0950912c7622f551d0e
Nayeem-Hasan <nayeemhasanpr@gmail.com>
2025-08-26 10:59:56 +0600

    working on Login Modal

--- src/components/main/reuseable/Navbar/Navbar.jsx
+++ src/components/main/reuseable/Navbar/Navbar.jsx
@@ -17,6 +17,9 @@ import {
 } from "@/components/ui/dropdown-menu";
 import Hamburger from "./Hamburger/Hamburger";
 import MobileNav from "./MobileNav";
+import LoginModal from "../Modals/LoginModal/LoginModal";
+
+import { Dialog } from "@/components/ui/dialog";
 
 const navLinks = [
   {
@@ -31,59 +34,66 @@ const navLinks = [
 
 const Navbar = () => {
   const [showMobileMenu, setShowMobileMenu] = useState(false);
-
-  // useEffect(() => {
-  //   console.log("showMobileMenu = ", showMobileMenu);
-  // }, [showMobileMenu]);
+  const [showLoginModal, setShowLoginModal] = useState(false);
 
   return (
-    <div className="bg-white backdrop-blur-md fixed top-0 left-0 right-0 border-b">
-      <div className="container min-h-[80px] flex items-center justify-between w-full gap-x-[20px] z-[10]">
-        <div className="flex gap-x-[15px] items-center">
-          <Image src={logo} className="w-36" alt="logo" />
+    <>
+      <div className="bg-white backdrop-blur-md fixed top-0 left-0 right-0 border-b">
+        <div className="container min-h-[80px] flex items-center justify-between w-full gap-x-[20px] z-[10]">
+          <div className="flex gap-x-[15px] items-center">
+            <Image src={logo} className="w-36" alt="logo" />
 
-          {navLinks?.length ? (
-            <nav className="hidden gap-6 lg:flex">
-              {navLinks?.map((item, index) => (
-                <Link key={index} href={item.href}>
-                  {item.title}
-                </Link>
-              ))}
-            </nav>
-          ) : null}
+            {navLinks?.length ? (
+              <nav className="hidden gap-6 lg:flex">
+                {navLinks?.map((item, index) => (
+                  <Link key={index} href={item.href}>
+                    {item.title}
+                  </Link>
+                ))}
+              </nav>
+            ) : null}
+          </div>
+
+          <div className="w-full max-w-[410px] flex-grow flex items-center gap-x-[10px]">
+            <SearchBar />
+            <DropdownMenu>
+              <DropdownMenuTrigger className="h-[40px] aspect-square rounded-full">
+                <div className="h-[40px] aspect-square border border-solid border-stock hover:border-primary rounded-full flex justify-center items-center cursor-pointer">
+                  <Icon
+                    icon="ep:user-filled"
+                    className="text-[26px] text-ash"
+                  />
+                </div>
+              </DropdownMenuTrigger>
+
+              <DropdownMenuContent align="end">
+                <DropdownMenuItem className="cursor-pointer">
+                  Sign Up
+                </DropdownMenuItem>
+
+                <DropdownMenuItem
+                  className="cursor-pointer"
+                  onClick={() => setShowLoginModal(true)}
+                >
+                  Login
+                </DropdownMenuItem>
+              </DropdownMenuContent>
+            </DropdownMenu>
+
+            <Hamburger
+              show={showMobileMenu}
+              callback={() => setShowMobileMenu(!showMobileMenu)}
+            />
+          </div>
         </div>
 
-        <div className="w-full max-w-[410px] flex-grow flex items-center gap-x-[10px]">
-          <SearchBar />
-          <DropdownMenu>
-            <DropdownMenuTrigger className="h-[40px] aspect-square rounded-full">
-              <div className="h-[40px] aspect-square border border-solid border-stock hover:border-primary rounded-full flex justify-center items-center cursor-pointer">
-                <Icon icon="ep:user-filled" className="text-[26px] text-ash" />
-              </div>
-            </DropdownMenuTrigger>
+        {showMobileMenu && navLinks && <MobileNav navLinks={navLinks} />}
 
-            <DropdownMenuContent align="end">
-              {/* <DropdownMenuLabel>My Account</DropdownMenuLabel>
-              <DropdownMenuSeparator /> */}
-              <DropdownMenuItem className="cursor-pointer">
-                Sign Up
-              </DropdownMenuItem>
-
-              <DropdownMenuItem className="cursor-pointer">
-                Login
-              </DropdownMenuItem>
-            </DropdownMenuContent>
-          </DropdownMenu>
-
-          <Hamburger
-            show={showMobileMenu}
-            callback={() => setShowMobileMenu(!showMobileMenu)}
-          />
-        </div>
+        <Dialog open={showLoginModal} onOpenChange={setShowLoginModal}>
+          <LoginModal />
+        </Dialog>
       </div>
-
-      {showMobileMenu && navLinks && <MobileNav navLinks={navLinks} />}
-    </div>
+    </>
   );
 };
 
