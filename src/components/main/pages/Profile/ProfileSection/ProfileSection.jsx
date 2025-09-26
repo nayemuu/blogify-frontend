74fa818752f4855e9e9ca0c497a6bd2ec38912ab
Nayem-Hasan <nayeemhasanpr@gmail.com>
2025-09-26 17:18:52 +0600

    profile page updated

--- src/components/main/pages/Profile/ProfileSection/ProfileSection.jsx
+++ src/components/main/pages/Profile/ProfileSection/ProfileSection.jsx
@@ -9,14 +9,14 @@ import "./ProfileSection.css";
 function ProfileSection(props) {
   const { isLoading, isError, isSuccess, data, error } = useProfileQuery();
 
-  useEffect(() => {
-    if (isSuccess) {
-      if (data?.data) {
-        console.log("data = ", data.data);
-        console.log("data = ", data.data.user.name);
-      }
-    }
-  }, [isSuccess]);
+  // useEffect(() => {
+  //   if (isSuccess) {
+  //     if (data?.data) {
+  //       console.log("data = ", data.data);
+  //       console.log("totalBlogs = ", data.data.user.totalBlogs);
+  //     }
+  //   }
+  // }, [isSuccess]);
 
   return isSuccess ? (
     <div className="container">
@@ -32,7 +32,10 @@ function ProfileSection(props) {
             )}
 
             <div className="text-[#757575] text-lg sm:text-xl md:text-2xl lg:text-2xl font-normal leading-[20.8px] sm:leading-[35.8px] mt-[6px] xxl:mt-[10px]">
-              Total Articles: <span className="text-primary">20</span>
+              Total Articles:{" "}
+              <span className="text-primary">
+                {data?.data?.user?.totalBlogs ? data.data.user.totalBlogs : 0}
+              </span>
             </div>
           </div>
         </div>
